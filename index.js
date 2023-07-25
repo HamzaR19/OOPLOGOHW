4632650cb82c8dcb58b03baad6347a1ebb5e542a
HamzaR19 <132932060+HamzaR19@users.noreply.github.com>
2023-07-25 16:22:44 -0500

    changed alot around trying to get the app functional

--- index.js
+++ index.js
@@ -1,93 +1,72 @@
-// setting variables and requiring Inquirer and FS
+// index.js
 
 const inquirer = require('inquirer');
 const fs = require('fs');
-const { Triangle, Circle } = require("./lib/shapes.js");
-const { log } = require('console');
-const SVG = require('./lib/svg.js')
+const { Triangle, Circle, Square } = require("./lib/shapes.js");
+const SVG = require('./lib/svg.js');
 
-var svgString ="";
-
-// const res = await inquirer.prompt
-
-
-class Svg{
-    constructor(){
-        this.textE
-    }
-
-
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if (err) throw err;
+    console.log("Generated logo.svg");
+  });
 }
 
-
-
-function writeToFile(fileName,res){
-
-    let
-}
-
-
-
-inquirer.prompt([
-      {
-        type: "input",
-        message:
-          "Choose 3 characters for the text of your logo",
-        name: "textName",
-      },
-      
-      {
-        type: "input",
-        message:
-          "Choose desired text color",
-        name: "textColor",
-      },
-      
-      {
-        type: "list",
-        message: "Please select a shape for your logo",
-        choices: ["Triangle", "Square", "Circle"],
-        name: "logoShape",
-      },
-     
-      {
-        type: "input",
-        message:
-          "Please select shape color",
-        name: "shapeColor",
-      },
-    ])
-
-    
-    .then((res) => {
-        // if (res.text.length > 3) {
-        //     console.log("Must enter a value of no more than 3 characters");
-        //     prompt();
-        // } else {
-        //     writeToFile("logo.svg", res);
-        // }
-        console.log("result", res);
-      
-        let shape;
-        switch (res.logoShape) {
-          case "circle":
-            shape = new Circle();
-            break;
-
-          // case "square":
-          //   shape = new Square();
-          //   break;
-
-          default:
-            shape = new Triangle();
-            break;
+inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "Choose 3 characters for the text of your logo",
+      name: "textName",
+      validate: function (input) {
+        if (input.length <= 3) {
+          return true;
         }
-        shape.setColor(shapeColor);
+        return "Must enter a value of no more than 3 characters";
+      },
+    },
 
-        const svg = new SVG();
+    {
+      type: "input",
+      message: "Choose desired text color",
+      name: "textColor",
+    },
 
-        
-    })
-    
-    // prompt();
- 
+    {
+      type: "list",
+      message: "Please select a shape for your logo",
+      choices: ["Triangle", "Circle", "Square"],
+      name: "logoShape",
+    },
+
+    {
+      type: "input",
+      message: "Please select shape color",
+      name: "shapeColor",
+    },
+  ])
+
+  .then((res) => {
+    const { textName, textColor, logoShape, shapeColor } = res;
+
+    let shape;
+    switch (logoShape) {
+      case "Circle":
+        shape = new Circle();
+        break;
+      case "Square":
+        shape = new Square();
+        break;
+      default:
+        shape = new Triangle();
+        break;
+    }
+    shape.setColor(shapeColor);
+
+    const svg = new SVG();
+    svg.setText(textName, textColor);
+    svg.setShape(shape);
+
+    const svgString = svg.render();
+    writeToFile("logo.svg", svgString);
+  });
